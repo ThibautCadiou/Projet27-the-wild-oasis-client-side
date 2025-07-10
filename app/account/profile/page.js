8bc5d5f2d52eb104bcb72cb49f1eb6aee4a8437d
SamGoldFisher-02 <105599905+SamGoldFisher-02@users.noreply.github.com>
2025-07-10 15:27:43 +0200

    ajout de la total layout

--- app/account/profile/page.js
+++ app/account/profile/page.js
@@ -1,6 +1,9 @@
 import SelectCountry from '@/app/_components/SelectCountry';
 import Image from 'next/image';
 
+export const metadata = {
+  title: 'Update profile',
+};
 export default function Page() {
   // CHANGE
   const countryFlag = 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg';
@@ -35,7 +38,7 @@ export default function Page() {
         <div className='space-y-2'>
           <div className='flex items-center justify-between'>
             <label htmlFor='nationality'>Where are you from?</label>
-            <Image src={countryFlag} alt='Country flag' className='h-5 rounded-sm' />
+            <img src={countryFlag} alt='Country flag' className='h-5 rounded-sm' />
           </div>
 
           <SelectCountry
