ef1c49413f78a2daa6164ac9458510ed8b023afd
SamGoldFisher-02 <105599905+SamGoldFisher-02@users.noreply.github.com>
2025-07-16 15:30:23 +0200

    ajout du filtrage des cabines

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,19 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
 import Spinner from '../_components/Spinner';
+import Filter from '../_components/Filter';
+
+// pour que la page soit dynamqiue et pour Opt Out la gestion du cache
+// export const revalidate = 3600;
+// export const revalidate = 0;
 
 export const metadata = {
   title: 'Cabins',
 };
 
-function Page() {
+function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? 'all';
+
   return (
     <div>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>Our Luxury Cabins</h1>
@@ -17,8 +24,12 @@ function Page() {
         own little home away from home. The perfect spot for a peaceful, calm vacation. Welcome to
         paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className='flex justify-end'>
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
