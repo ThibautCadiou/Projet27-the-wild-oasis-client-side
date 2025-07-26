4402a6e60182bd439056012ef659988da3ac9830
SamGoldFisher-02 <105599905+SamGoldFisher-02@users.noreply.github.com>
2025-07-26 19:19:17 +0200

    ajout de la connection avec google

--- app/account/page.js
+++ app/account/page.js
@@ -1,13 +1,16 @@
 import Navigation from '../_components/Navigation';
+import { auth } from '../_lib/auth';
 
 export const metadata = {
   title: 'Account',
 };
 
-function Home() {
+async function Home() {
+  const session = await auth();
+  const firstname = session.user.name.split(' ').at(0);
   return (
     <div className=''>
-      <h2 className='font-semibold text-2xl text-accent-400 mb-7'>Welcome JONAS</h2>
+      <h2 className='font-semibold text-2xl text-accent-400 mb-7'>Welcome, {firstname}</h2>
     </div>
   );
 }
